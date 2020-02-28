ad036fcf2ea3dcf8e403d72886f9b0a725d903ae
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-28 15:46:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,12 +7,12 @@ def create_project_hash
   
   kickstarter = Nokogiri::HTML(html)
   
-  projects = kickstarter.css("li.project.grid_4")
-  #projects.css("h2.bbcard_name strong a").text = All texts from this section on
-  #projects.css("h2.bbcard_name strong a").first.text = Moby Dick: An Oratorio
-  #projects.css("div.project-thumbnail a img").attribute("src").value  = the web page source Image
-  # projects.css("p.bbcard_blurb").text all descriptions of all the projects in this current space of projects
-  #projects.css(".location-name").text all locations of all projects
+  project = kickstarter.css("li.project.grid_4")
+  #project.css("h2.bbcard_name strong a").text = All texts from this section on
+  #project.css("h2.bbcard_name strong a").first.text = Moby Dick: An Oratorio
+  #project.css("div.project-thumbnail a img").attribute("src").value  = the web page source Image
+  # project.css("p.bbcard_blurb").text all descriptions of all the projects in this current space of projects
+  #project.css(".location-name").text all locations of all projects
   binding.pry
   
 end
