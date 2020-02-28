1eb5d5245ec80a6157875707e64d3f2bb14c9be4
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-28 15:34:50 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,10 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   
   projects = kickstarter.css("li.project.grid_4")
+  #projects.css("h2.bbcard_name strong a").text = All texts from this section on
   #projects.css("h2.bbcard_name strong a").first.text = Moby Dick: An Oratorio
+  #projects.css("div.project-thumbnail a img").attribute("src").value  Oratorio
+  
   
   binding.pry
   
