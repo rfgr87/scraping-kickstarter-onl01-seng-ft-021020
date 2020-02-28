d868fbe5a156e9c0538aeddc36c9ec4406391fa9
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-28 15:14:01 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,12 @@
 require 'nokogiri'
+require 'pry'
 
 def create_project_hash
 
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
-  
+  binding.pry
 end
+
+create_project_hash
